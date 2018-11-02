01934c602590343ca0f6b332b49c2e0dd573b58e
TacoTheDank <SkytkRSfan3895@gmail.com>
2018-11-01 20:34:22 -0400

    code

--- app/src/main/java/de/perflyst/batterycalibration/MainActivity.java
+++ app/src/main/java/de/perflyst/batterycalibration/MainActivity.java
@@ -1,8 +1,7 @@
 package de.perflyst.batterycalibration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -20,43 +19,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         onClickCalibrate();
+    }
 
-        }
+    public void onClickCalibrate() {
+        calibrate_button = findViewById(R.id.calibrate_button);
 
-        public void onClickCalibrate() {
-            calibrate_button = (Button)findViewById(R.id.calibrate_button);
+        calibrate_button.setOnClickListener(view -> {
+            try {
+                Process su = Runtime.getRuntime().exec("su");
+                DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
 
-            calibrate_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try {
-                        Process su = Runtime.getRuntime().exec("su");
-                        DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+                File file_batt_reset = new File("/sys/class/power_supply/battery/batt_reset_soc");
 
-                        File file_batt_reset = new File("/sys/class/power_supply/battery/batt_reset_soc");
-
-                        if(file_batt_reset.exists()) {
-                            outputStream.writeBytes("echo 1 > /sys/class/power_supply/battery/batt_reset_soc\n");
-                            outputStream.flush();
-                            outputStream.writeBytes("dumpsys batterystats --reset\n");
-                            outputStream.flush();
-                            outputStream.writeBytes("exit\n");
-                            outputStream.flush();
-                            su.waitFor();
-                        } else {
-                            outputStream.writeBytes("dumpsys batterystats --reset\n");
-                            outputStream.flush();
-                            outputStream.writeBytes("exit\n");
-                            outputStream.flush();
-                            su.waitFor();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Toast.makeText(getApplicationContext(), "Calibration successful", Toast.LENGTH_LONG).show();
+                if (file_batt_reset.exists()) {
+                    outputStream.writeBytes("echo 1 > /sys/class/power_supply/battery/batt_reset_soc\n");
+                    outputStream.flush();
+                    outputStream.writeBytes("dumpsys batterystats --reset\n");
+                    outputStream.flush();
+                    outputStream.writeBytes("exit\n");
+                    outputStream.flush();
+                    su.waitFor();
+                } else {
+                    outputStream.writeBytes("dumpsys batterystats --reset\n");
+                    outputStream.flush();
+                    outputStream.writeBytes("exit\n");
+                    outputStream.flush();
+                    su.waitFor();
                 }
-            });
-        }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(getApplicationContext(), "Calibration successful!", Toast.LENGTH_LONG).show();
+        });
+    }
 }
